dc5f1fed3acdd363776ce032d1eb9faf5ea977cf
Jonatas <84332883+Jonatas-de-Melo@users.noreply.github.com>
2024-10-05 11:31:19 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{color: '#fff'}}>Bem Vindo!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+});
---
+++ index.js
@@ -0,0 +1,7 @@
+import { registerRootComponent } from 'expo';
+
+import App from './App';
+
+import Login from './src/pages/Login';
+
+registerRootComponent(Login);
